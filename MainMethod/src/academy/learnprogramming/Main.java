23a994ecd7d939577c7ca82ce50434026b5ce72b
AritroB <abasuusa2010@gmail.com>
2020-04-13 17:27:17 -0500

    added a println in main method

--- MainMethod/src/academy/learnprogramming/Main.java
+++ MainMethod/src/academy/learnprogramming/Main.java
@@ -3,6 +3,7 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-
+        // I'll do something nice here
+        System.out.println("Hola Amigos!!!");
     }
 }
