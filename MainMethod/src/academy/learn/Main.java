c6c2779a1b16abc4cd51823e3e9781a287dad81b
AritroB <abasuusa2010@gmail.com>
2020-04-15 21:27:18 -0500

    Arguments in Main.java

---
+++ MainMethod/src/academy/learn/Main.java
@@ -0,0 +1,15 @@
+package academy.learn;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("args-size = " + args.length);
+
+        for(int i=0; i< args.length; i++)
+        {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+    }
+
+
+}
--- MainMethod/src/academy/learnprogramming/Main.java
+++
@@ -1,9 +0,0 @@
-package academy.learnprogramming;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // I'll do something nice here
-        System.out.println("Hola Amigos!!!");
-    }
-}
