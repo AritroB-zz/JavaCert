9c9bbb818d9ef3018ae399f0de5b28d56828a261
AritroB <abasuusa2010@gmail.com>
2020-04-17 18:44:47 -0500

    imported Random

--- PackagesAndImports/src/academy/learnprogramming/ImportExample.java
+++ PackagesAndImports/src/academy/learnprogramming/ImportExample.java
@@ -1,4 +1,13 @@
 package academy.learnprogramming;
 
+import java.util.Random;
+import java.lang.System;
+
 public class ImportExample {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(5));
+
+    }
+
 }
