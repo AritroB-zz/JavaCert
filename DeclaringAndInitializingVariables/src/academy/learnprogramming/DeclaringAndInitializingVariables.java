827a8217c9ca2eec87eedb7cb551477c618005c8
AritroB <abasuusa2010@gmail.com>
2020-04-18 14:22:14 -0500

    Declaring and Initializing Variables

---
+++ DeclaringAndInitializingVariables/src/academy/learnprogramming/DeclaringAndInitializingVariables.java
@@ -0,0 +1,43 @@
+package academy.learnprogramming;
+
+public class DeclaringAndInitializingVariables {
+
+    public static void main(String[] args) {
+        int myNumber;
+        //System.out.println("myNumber= " + myNumber);
+
+        myNumber = 10;
+        System.out.println("myNumber= " + myNumber);
+
+        //one line
+
+        double myDouble = 7.50;
+        System.out.println("myDouble=" + myDouble);
+
+        float myFloat1, myFloat2, myFloat3;
+        float myFloat4; float myFloat5;
+
+        float myFloat6 = 5f, myFloat7 = 10f, myFloat8;
+        boolean b1, b2;
+
+        //double d1, double d2; //does not compile
+
+        int i1;
+        int i2;
+
+ //       int int = 10; //does not compile
+
+        float okFloat;
+        double $Ok2Double1;
+        double _alsoDouble;
+        float __OkButNotNice$_123;
+
+        // illegal examples
+        // double 3point;
+        // double my@street;
+        // float *$coffee;
+        // float double;
+        // double publicl
+
+    }
+}
