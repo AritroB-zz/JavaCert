f07b6b1502c79e8fed4c77ba9c6e4eb29d2b7223
AritroB <abasuusa2010@gmail.com>
2020-04-18 16:12:00 -0500

    Ordering Elements

---
+++ OrderingElements/src/academy/learnprogramming/OrderingElements.java
@@ -0,0 +1,24 @@
+//import java.util.Random;
+
+package academy.learnprogramming;
+
+//import java.util.*;
+
+//package academy;
+
+//int myInt = 10;
+
+//public void myMethod() {}
+
+class OtherClass {}
+
+class Class2{}
+
+public class OrderingElements {
+    int i = 10;
+
+    public static void main(String[] args) {
+        int i2 = 20;
+
+    }
+}
