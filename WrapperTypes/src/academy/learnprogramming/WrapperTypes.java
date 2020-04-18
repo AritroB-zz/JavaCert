710be203c5b04502cb3d1aa1b958e48e4d18d749
AritroB <abasuusa2010@gmail.com>
2020-04-18 17:38:44 -0500

    Wrapper Types and Boxing

---
+++ WrapperTypes/src/academy/learnprogramming/WrapperTypes.java
@@ -0,0 +1,38 @@
+package academy.learnprogramming;
+
+public class WrapperTypes {
+
+    public static void main(String[] args) {
+        int myInt = 10;
+        Integer myInteger = 10;
+        Integer myInteger2 = 20;
+        Integer myInteger3 = Integer.valueOf(10);
+        Integer myInteger4 = Integer.parseInt("3");
+        Integer myInteger5 = null;
+        // int myInt2 = null;
+
+        System.out.println("myInteger= " + myInteger);
+        System.out.println("myInteger2= " + myInteger2);
+        System.out.println("myInteger3= " + myInteger3);
+        System.out.println("myInteger4= " + myInteger4);
+        System.out.println("myInteger5= " + myInteger5);
+
+        // converting wrapper to primitive is called unboxing
+        int myInt3 = myInteger3; //unboxing
+        // int myInt4 = myInteger5; //primitives cant contain null
+
+        // boxing is converting primitive to wrapper
+        Integer myInteger6 = new Integer(10);
+        Integer myInteger7 = myInt;
+
+        printSum(1, 5); // autoboxing
+        printSum(myInteger, myInteger2);
+
+    }
+
+    private static void printSum(Integer first, Integer second) {
+        Integer sum = first + second;
+        System.out.println("sum= " + (first + second));
+
+    }
+}
